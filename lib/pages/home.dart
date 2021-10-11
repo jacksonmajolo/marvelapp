a895702588d8c792b3964bbd5072972d3613a0fd
Jackson Majolo <jacksonmajolo@gmail.com>
2021-10-11 20:45:44 -0300

    Responsive widgets and Platform widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:marvelapp/configs/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:marvelapp/widgets/responsive.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -23,6 +24,24 @@ class _HomePageState extends State<HomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
+      body: ResponsiveLayout(
+        mobileLayout: _HomePageMobile(),
+        tabletLayout: _HomePageTablet(),
+      ),
     );
+  }
+}
+
+class _HomePageMobile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Mobile'));
+  }
+}
+
+class _HomePageTablet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Tablet'));
   }
 }
---
+++ lib/widgets/responsive.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart'
+    show Widget, StatelessWidget, BuildContext, Key, LayoutBuilder;
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileLayout;
+  final Widget? tabletLayout;
+  final Widget? desktopLayout;
+
+  const ResponsiveLayout(
+      {Key? key,
+      required this.mobileLayout,
+      this.tabletLayout,
+      this.desktopLayout})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 768.0) {
+        return this.mobileLayout;
+      } else if (constraints.maxWidth >= 768.0 &&
+          constraints.maxWidth > 1440.0) {
+        return (this.tabletLayout ?? this.mobileLayout);
+      } else {
+        return (this.desktopLayout ?? (this.tabletLayout ?? this.mobileLayout));
+      }
+    });
+  }
+}
