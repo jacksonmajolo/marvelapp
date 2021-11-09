63899ec37d09055a6e2e95031a313e20d10e5da1
Jackson Majolo <jacksonmajolo@gmail.com>
2021-11-08 22:09:04 -0300

    app error handling

---
+++ lib/configs/error.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import 'dart:developer';
+
+enum AppError { dart, flutter }
+
+class AppErrorReport {
+  static void report(AppError appError,
+      {Object? error, StackTrace? stackTrace}) {
+    log('Error on $appError', error: error, stackTrace: stackTrace);
+  }
+}
